8cfba8cf9afaccf7aef5c54c663f0ccb514d2013
BalaadityaPatanjali <balaaditya.0808.com>
2025-07-14 19:50:25 +0530

    frontend changes

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\aadi\Desktop\aadi\code\onlinexamination\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
